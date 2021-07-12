513937a5b94706b3d22cb21446cd7414a8c4e5d1
juncollin <juncollin@gmail.com>
2021-07-13 00:01:47 +0900

    add plane

--- t20210712/ContentView.swift
+++ t20210712/ContentView.swift
@@ -20,12 +20,21 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+//        // Load the "Box" scene from the "Experience" Reality File
+//        let boxAnchor = try! Experience.loadBox()
+//
+//        // Add the box anchor to the scene
+//        arView.scene.anchors.append(boxAnchor)
+
+        let anchor = AnchorEntity()
+        arView.scene.anchors.append(anchor)
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
+        // ③ Plane生成とVideoMaterial設定
+        let planeMesh = MeshResource.generatePlane(width: 0.8, height: 0.45)
+        let material = VideoMaterial(avPlayer: player)
+        let planeModel = ModelEntity(mesh: planeMesh, materials: [material])
+        planeModel.position = SIMD3<Float>(0.0, 0.0, -1.0)
+        anchor.addChild(planeModel)
         return arView
         
     }
